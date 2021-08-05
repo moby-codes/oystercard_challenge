e283336c21647335e63ac0f0054ce0762ab5ac2d
moby-codes <mubashar.jaffri@gmail.com>
2021-08-05 15:16:11 +0100

    ch12 journey history can now be saved in a hash, added private method to save journey

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,37 +1,45 @@
 class OysterCard
     
-    attr_reader :balance, :entry_station
-    MAX_BALANCE = 90
-    MIN_BALANCE = 1
-    MIN_FARE = 1
-    def initialize(balance = 0)
-      @balance = balance
-      @entry_station = nil
-    end
-   
-    def top_up(credit)
-      raise 'Cannot exceed maximum amount £90' if @balance + credit >= MAX_BALANCE
-      @balance += credit
-    end
+  attr_reader :balance, :entry_station, :journey
+  MAX_BALANCE = 90
+  MIN_BALANCE = 1
+  MIN_FARE = 1
+  def initialize(balance = 0)
+    @balance = balance
+    @entry_station = nil
+    @journey = []
 
-    def in_journey?
-      !!entry_station
-    end
-    
-    def touch_in(station)
-      raise "insufficient balance" if @balance < MIN_BALANCE
-      if @balance >= MIN_BALANCE
-        @entry_station
-      end
-      @entry_station = station
-    end
+  end
+ 
+  def top_up(credit)
+    raise 'Cannot exceed maximum amount £90' if @balance + credit >= MAX_BALANCE
+    @balance += credit
+  end
 
-    def touch_out
-      deduct(MIN_FARE)
-      @entry_station = nil
-    end  
-    private
-    def deduct(amount)
-      @balance -= amount
-    end  
+  def in_journey?
+    !!entry_station
+  end
+  
+  def touch_in(station)
+    raise "insufficient balance" if @balance < MIN_BALANCE
+    if @balance >= MIN_BALANCE
+      @entry_station
+    end
+    @entry_station = station
+  end
+
+  def touch_out(station)
+    save_journey(@entry_station, station)
+    @entry_station = nil  
+    deduct(MIN_FARE)
+  end  
+  
+  private
+  def deduct(amount)
+    @balance -= amount
+  end  
+  def save_journey(station1, station2)
+    @journey.push({entry: station1, exit: station2})
+  end
 end
+
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -3,6 +3,7 @@ require 'OysterCard'
 describe OysterCard do
 
   let (:station) { double :station}
+  let (:journey) { {entry: station, exit: station } }
 
     it 'should show balance of zero' do
       expect(subject.balance).to eq(0)
@@ -21,16 +22,6 @@ describe OysterCard do
        subject.top_up(89) 
        expect { subject.top_up(1) }.to raise_error('Cannot exceed maximum amount £90')
     end
-    
-    # describe 'Test the deduct method exists' do
-    #   it { is_expected.to respond_to(:deduct) }
-    # end
-    
-    # it 'Deducts £2 from the balance when deduct is called' do
-    #   subject.top_up(4)
-    #   subject.send(:deduct, amount) 
-    #   expect { subject.deduct }.to change { subject.balance }.from(4).to(2)
-    # end    
 
     it 'checks if you are in journey' do
       expect(subject.in_journey?).to eq(false) 
@@ -43,14 +34,14 @@ describe OysterCard do
     end  
     
     it 'checks if you have touched out' do
-      subject.touch_out
+      subject.touch_out(station)
       expect(subject.in_journey?).to eq(false)
     end
 
     it 'checks the state of the journey' do
       subject.top_up(5)
       subject.touch_in(station)
-      subject.touch_out
+      subject.touch_out(station)
       expect(subject).not_to be_in_journey
     end
     it "checks for min amount of £1 when touch_in" do
@@ -59,7 +50,7 @@ describe OysterCard do
     it "deducts money on touch_out" do
       subject.top_up(5)
       subject.touch_in(station)
-      expect{subject.touch_out}.to change{subject.balance}.by -1
+      expect{subject.touch_out(station)}.to change{subject.balance}.by -1
     end
 
     it 'stores the entry station' do
@@ -68,9 +59,15 @@ describe OysterCard do
       expect(subject.entry_station).to eq station
     end
 
+    it 'initializes with an empty journey array' do
+      expect(subject.journey).to eq([])
+    end 
 
-
+    it 'checks if the journey history is saved' do
+      subject.top_up(5)
+      subject.touch_in(station)
+      subject.touch_out(station)
+      expect(subject.journey).to include(journey)
+    end
 end
-
-
 
