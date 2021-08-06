8c8db27a423cf6b89b1a17cc7adfb1088705562a
moby-codes <mubashar.jaffri@gmail.com>
2021-08-06 15:54:26 +0100

    ch 14 - moved over in journey created end journey, removed in journey from oystercard

--- lib/OysterCard.rb
+++ lib/OysterCard.rb
@@ -1,13 +1,13 @@
 class OysterCard
     
-  attr_reader :balance, :entry_station, :journey
+  attr_reader :balance, :entry_station, :journey_log, :journey
   MAX_BALANCE = 90
   MIN_BALANCE = 1
   MIN_FARE = 1
   def initialize(balance = 0)
     @balance = balance
-     @entry_station = nil
-     @journey = []
+     @journey_log = []
+     @journey = nil
   end
  
   def top_up(credit)
@@ -15,23 +15,19 @@ class OysterCard
     @balance += credit
   end
 
-   def in_journey?
-    !!entry_station
-   end
+  #  def in_journey?
+  #   !!entry_station
+  #  end
   
   def touch_in(station)
     raise "insufficient balance" if @balance < MIN_BALANCE
-    if @balance >= MIN_BALANCE
-      @entry_station
-    end
-    @entry_station = station
+    @journey = Journey.new(station)
   end
-
 
   def touch_out(station)
     save_journey(@entry_station, station)
-    @entry_station = nil  
-    deduct(MIN_FARE)
+    @journey.end_journey 
+    deduct(@journey.fare)
   end  
   
   private
@@ -39,7 +35,7 @@ class OysterCard
     @balance -= amount
   end  
   def save_journey(station1, station2)
-    @journey.push({entry: station1, exit: station2})
+    @journey_log.push({entry: station1, exit: station2})
   end
 end
 
--- lib/journey.rb
+++ lib/journey.rb
@@ -4,22 +4,25 @@ class Journey
 
   #attr_reader :entry_station, :journeys
 
-  # def initialize
-  #   @entry_station = nil
-  #   @journeys = []
-  # end
+  def initialize(entry_station = nil)
+    @entry_station = entry_station
+    @exit_station = nil
+  end
 
-  # def in_journey?
-  #   !!entry_station
-  # end
+  def in_journey?
+    !!entry_station
+  end
 
-  # def touch_in(station)
-  #   raise "insufficient balance" if @balance < MIN_BALANCE
-  #   if @balance >= MIN_BALANCE
-  #     @entry_station
-  #   end
-  #   @entry_station = station
-  # end
+  def end_journey(station)
+    save_journey(@entry_station, station)
+    @entry_station = nil  
+    deduct(MIN_FARE)
+  end  
+  
+  def fare 
+  
+  end
+
 
 
 end
